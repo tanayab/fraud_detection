15c1af6d56d60c739ca8ee1abc6b75d087148151
tanayab <tnbwd5@gmail.com>
2023-11-13 10:21:25 +0530

    changes

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,2 @@
 
 print("adding all the code related to data preprocessing in fraud detection")
-
-a=5
-b=7
-
-def sum(a,b):
-    c=a+b
-    return a 
-
-print("added a new function sum which is not correct")
